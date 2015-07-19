73ef5af3d12797d2312b3bbaf8a0db7ca64c77b2
TurBoss <j.l.toledano.l@gmail.com>
2015-07-19 20:16:48 +0200

    Icon

--- setup.py
+++ setup.py
@@ -1,19 +1,25 @@
+import sys
 from cx_Freeze import setup, Executable
 
 
+base = None
+if sys.platform == 'win32':
+	base = 'Win32GUI'
+	
 executables = [
-    Executable("App1.py",
-               #icon="logo.ico",
-               base = "Win32GUI",
-               appendScriptToExe=True,
-               appendScriptToLibrary=False,
-               )
+	Executable("App1.py",
+				icon="tp.ico",
+				base = base,
+				appendScriptToExe=True,
+				appendScriptToLibrary=False,
+				)
 ]
 
 buildOptions = dict(create_shared_zip=False,)
+
 setup(name="FF7 patcher",
-      version="0.1",
-      description="Create Patchs and patch files",
-      options=dict(build_exe=buildOptions),
-      executables=executables,
-      )
+		version="0.1",
+		description="Create Patchs and patch files",
+		options=dict(build_exe=buildOptions),
+		executables=executables,
+		)
